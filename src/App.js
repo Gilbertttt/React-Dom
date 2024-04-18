dc87def682a4501ae51f0a5431c14664dfe36d79
Gilbertttt <umorengilbert17@gmail.com>
2024-04-18 05:01:05 +0100

    learn react-dom

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //React Router-Dom
+  return (
+    <div className="App">
+      <h1>My name is Gilbert</h1>
+    </div>
+  );
+}
+
+export default App;
